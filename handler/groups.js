07ec655b59e0739c700417b1f5b4435ccb91f574
dulzxx <137727789+dulzxx@users.noreply.github.com>
2023-07-04 01:10:17 +0700

    Delete handler directory

--- handler/groups.js
+++
@@ -1,8 +0,0 @@
-const logging = require("../lib/logging");
-const saveUsers = require("../lib/saveUsers");
-
-module.exports = ({ reybot, g }) => {
-  const userId = g.participants[0];
-  saveUsers({ userId });
-  return;
-};
